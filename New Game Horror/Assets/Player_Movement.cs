6872001e69e11bc3bb3102f2971c1eeb2370e3c0
sredondo34 <sredondo34@gmail.com>
2018-02-22 02:43:15 -0600

    Basic stuff
    Just some basic additions like player model and making a basic hallway for the player to maneuver.

---
+++ New Game Horror/Assets/Player_Movement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    float speed = 0.5f;
+    public Rigidbody body;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        body = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W)) //Makes the player go forwards
+        {
+
+            body.AddForce(transform.forward * speed, ForceMode.Impulse);
+
+
+        }
+
+        if (Input.GetKey(KeyCode.D)) //Makes the player go right
+        {
+
+            body.AddForce(transform.right * speed, ForceMode.Impulse);
+
+        }
+
+        if (Input.GetKey(KeyCode.A)) //Makes the player go left
+        {
+
+            body.AddForce(-transform.right * speed, ForceMode.Impulse);
+
+        }
+
+        if (Input.GetKey(KeyCode.S)) //Makes the player go backwards
+        {
+
+            body.AddForce(-transform.forward * speed, ForceMode.Impulse);
+
+        }
+
+    }
+}
